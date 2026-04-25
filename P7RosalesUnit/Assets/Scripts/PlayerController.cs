934cb331584dc64d5a633cac9ea08573c2dc94dd
733482-code <733482@student.dusd.net>
2026-04-25 12:20:32 -0700

    working on final challenge
    challenges

--- P7RosalesUnit/Assets/Scripts/PlayerController.cs
+++ P7RosalesUnit/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,20 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    private GameObject tmpRocket;
+    private Coroutine powerupCountdown;
     private float powerupStrength = 15.0f;
     public float speed = 5.0f;
+    public float hangTime;
+    public float smashSpeed;
+    public float explosionForce;
+    public float explosionRadius;
     public bool hasPowerup = false;
+    bool smashing = false;
+    float floorY;
+    public PowerUpType currentPowerUp = PowerUpType.None;
     public GameObject powerupIndicator;
+    public GameObject rocketPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,6 +33,17 @@ public class PlayerController : MonoBehaviour
         playerRb.AddForce(focalPoint.transform.forward * speed * (float)Input.GetAxis("Vertical"));
 
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+
+        if (currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.F))
+        {
+            LaunchRockets();
+        }
+
+        if (currentPowerUp == PowerUpType.Smash && Input.GetKeyDown(KeyCode.Space) && !smashing)
+        {
+            smashing = true;
+            StartCoroutine(Smash());
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -30,9 +51,15 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            currentPowerUp = other.gameObject.GetComponent<PowerUp>().powerUpType;
             powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
-            StartCoroutine(PowerupCountdownRoutine());
+
+            if (powerupCountdown != null)
+            {
+                StopCoroutine(powerupCountdown);
+            }
+            powerupCountdown = StartCoroutine(PowerupCountdownRoutine());
         }
     }
 
@@ -40,18 +67,57 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        currentPowerUp = PowerUpType.None;
         powerupIndicator.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        if (collision.gameObject.CompareTag("Enemy") && currentPowerUp == PowerUpType.Pushback)
         {
             Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
 
             enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
-            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            Debug.Log("Player Collided with: " + collision.gameObject.name + " with powerup set to " + currentPowerUp.ToString());
         }
+    }
+
+    void LaunchRockets()
+    {
+        foreach (var enemy in FindObjectsOfType<Enemy>())
+        {
+            tmpRocket = Instantiate(rocketPrefab, transform.position + Vector3.up, Quaternion.identity);
+            tmpRocket.GetComponent<RocketBehaviour>().Fire(enemy.transform);
+        }
+    }
+
+    IEnumerator Smash()
+    {
+        var enemies = FindObjectsOfType<Enemy>();
+
+        floorY = transform.position.y;
+
+        float jumpTime = Time.time + hangTime;
+
+        while (Time.time < jumpTime)
+        {
+            playerRb.linearVelocity = new Vector2(playerRb.angularVelocity.x, smashSpeed);
+            yield return null;
+        }
+
+        while (transform.position.y > floorY)
+        {
+            playerRb.angularVelocity = new Vector2(playerRb.angularVelocity.x, -smashSpeed * 2);
+            yield return null;
+        }
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (enemies[i] != null)
+                enemies[i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce, transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+        }
+
+        smashing = false;
     }
 }
--- P7RosalesUnit/Assets/Scripts/RocketBehaviour.cs
+++ P7RosalesUnit/Assets/Scripts/RocketBehaviour.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class RocketBehaviour : MonoBehaviour
 {
     private Transform target;
-    private float speed = -15.0f;
+    private float speed = 15.0f;
     private bool homing;
     private float rocketStrength = 15.0f;
     private float aliveTimer = 5.0f;
@@ -27,8 +27,22 @@ public class RocketBehaviour : MonoBehaviour
 
     public void Fire(Transform newTarget)
     {
-        target = homingTarget;
+        target = newTarget;
         homing = true;
         Destroy(gameObject, aliveTimer);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (target != null)
+        {
+            if (col.gameObject.CompareTag(target.tag))
+            {
+                Rigidbody targetRigidbody = col.gameObject.GetComponent<Rigidbody>();
+                Vector3 away = -col.contacts[0].normal;
+                targetRigidbody.AddForce(away * rocketStrength, ForceMode.Impulse);
+                Destroy(gameObject);
+            }
+        }
     }
 }
